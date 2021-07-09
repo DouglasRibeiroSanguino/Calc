a2936942b83540cd5468fe278ca743b3f8467f66
perkles <douglasribeirosanguino3@gmail.com>
2021-07-09 20:42:34 -0300

    Hello.kt

---
+++ Hello.kt
@@ -0,0 +1,50 @@
+//Calculadora que detecta o tipo de equação que você deseja!
+//Aut: Douglas Ribeiro Sanguino
+
+fun main() {
+    println("Olá, seja muito Bem-Vindo a nossa calculadora!")
+    println("Considere a seguir os números para o tipo de operação!")
+    println("Por Favor digite o tipo de operação que você deseja:")
+    println(
+        "1 - Para Soma ( + )" + "\n2 - Para Subtração ( - )" + "\n3 - Para Multiplicação ( * )"
+                + "\n4 - Para Divisão ( / )"
+    )
+    val operation = readLine()!!.toInt()
+    val numberToText = when (operation) {
+        1 -> "Soma"
+        2 -> "Subtração"
+        3 -> "Multiplicação"
+        4 -> "Divisão"
+        else -> System.exit(0)
+    }
+    println("Hum, " + numberToText + ", ok...")
+
+    val TextOperation = when (operation){
+        1 -> "Somar"
+        2 -> "Subtrair"
+        3 -> "Multiplicar"
+        4 -> "Dividir"
+        else -> ""
+    }
+    val operation2 = when (operation){
+        1 -> "+"
+        2 -> "-"
+        3 -> "*"
+        4 -> "/"
+        else -> ""
+    }
+    println("Por Favor digite o primeiro número que você deseja " + TextOperation + ":")
+    val n1 = readLine()!!.toInt()
+    println("Por favor digite o segundo número que você deseja " + TextOperation + ":")
+    val n2 = readLine()!!.toInt()
+    var total = when (operation){
+        1 -> n1+n2
+        2 -> n1-n2
+        3 -> n1*n2
+        4 -> n1/n2
+        else -> ""
+    }
+    println("A "+ numberToText + " entre " + n1 + operation2 + n2 + " é de: " + total)
+
+
+}
